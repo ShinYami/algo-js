227e36d8298d2f1d14d456e6c47fdaefadf4f627
Conti Loris <loriisconti@gmail.com>
2021-01-18 17:05:19 +0100

    add 2.2 2.3
    exo 2.3.1 done but need to fix 2.2

---
+++ Exo-2.2.js
@@ -0,0 +1,11 @@
+const readlineSync = require("readline-sync");
+
+let min = new Number(readlineSync.question("Give me a number"));
+let max = new Number(readlineSync.question("Give me a second number"));
+
+let current = new Number(readlineSync.question("Give me your last number between your last two input"));
+
+if (current<max)
+    
+    console.log(Congratz)
+
