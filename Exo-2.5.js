3fbdb666abf9bd86150d1e667927f7b8cd09da4f
Conti Loris <loriisconti@gmail.com>
2021-01-18 22:41:58 +0100

    add 2.5

---
+++ Exo-2.5.js
@@ -0,0 +1,8 @@
+const readlineSync = require("readline-sync");
+
+let favNumber = readlineSync.question("What's your favorite number ?");
+
+while (favNumber!="42") {
+              favNumber = readlineSync.question("Wait, are you sure ? What's your favorite number ?");
+} 
+    console.log("Congratz!");
