76f5a34add57772b589cf9ffda10a8d9a8613d7d
Conti Loris <loriisconti@gmail.com>
2021-01-18 14:17:28 +0100

    1.7

---
+++ Exo-1.7.js
@@ -0,0 +1,26 @@
+const readlineSync = require("readline-sync");
+
+let var1 = readlineSync.question("What's your shoe size ?");
+
+let var2 = readlineSync.question("What's your birth year ?");
+
+let var3 = (var1*2);
+
+console.log(var3);
+
+console.log(var3+5);
+
+console.log((var3+5)*50);
+
+console.log((var3+5)*50-var2);
+
+console.log((var3+5)*50-var2);
+
+let var4 = ((var3+5)*50-var2);
+
+console.log(var4+1766);
+
+
+
+
+
