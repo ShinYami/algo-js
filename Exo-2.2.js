a8c2a278cdaf4d658a9f0462a7b6c2e0a016abd2
Conti Loris <loriisconti@gmail.com>
2021-01-18 23:06:09 +0100

    Update Exo-2.2.js

--- Exo-2.2.js
+++ Exo-2.2.js
@@ -1,11 +1,17 @@
 const readlineSync = require("readline-sync");
 
-let min = new Number(readlineSync.question("Give me a number"));
-let max = new Number(readlineSync.question("Give me a second number"));
+let min = new Number(readlineSync.question("Give me a small number"));
+let max = new Number(readlineSync.question("Give me a greater number"));
 
-let current = new Number(readlineSync.question("Give me your last number between your last two input"));
-
-if (current<max)
-    
-    console.log(Congratz)
+if (min>=max) {
+    console.log("Dude, how minimum can be greater than maximum? U kidding me?");
+}   else {
+    console.log("Thanks");
+    let current = new Number(readlineSync.question("Give me your last number between your last two numbers"));
+    if ((current>=max) || (current<=min)) {
+    console.log("Try again");
+  } else {
+    console.log("GoodJob"); 
+  }
+}
 
