eae86d93525d041fdc5ffc4bbaeffad9d187fbef
Conti Loris <loriisconti@gmail.com>
2021-01-19 10:40:57 +0100

    Create Exo-4.3.js

---
+++ Exo-4.3.js
@@ -0,0 +1,20 @@
+const readlineSync = require("readline-sync");
+
+function rand10(min, max) {
+    return Math.floor(Math.random() * 10) + 1;
+}
+
+function multiRand(n) {
+    let multi = [];
+    for (let i = 0; i < n; i++) {
+        multi.push(rand10());
+    }
+    return multi;
+}
+
+let number = new Number(
+    readlineSync.question("how many numbers do you want between 1 and 10 ? ")
+);
+
+console.log("Here is random numbers: ");
+for (e of multiRand(number)) console.log(e);
